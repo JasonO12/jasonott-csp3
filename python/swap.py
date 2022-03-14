db35257a29aa71eceab0e98077567025e48794d1
Jason Ott <89180255+JasonO12@users.noreply.github.com>
2022-03-14 02:10:18 +0000

    swap fix

--- python/swap.py
+++ python/swap.py
@@ -1,12 +1,16 @@
-def swapNum(x, y):
-    if x > y:
-        temp = x
-        x = y
-        y = temp
-    print(x, y)
+def swapNum(num1, num2):
+    if int(num1) > int(num2):
+        temp = num1
+        num1 = num2
+        num2 = temp
+    return(num1, num2)
 
 def test_swapNum():
-    num1 = int(input("input age 1: "))
-    num2 = int(input("input age 2: "))
-    x, y = swapNum(num1, num2)
-    print(x, y)
+    num1 = input("Input age 1: ")
+    num2 = input("Input age 2: ")
+    num1, num2 = swapNum(num1, num2)
+    print("Swapped:", num1, num2)
+
+
+if __name__ == "__main__":
+    test_swapNum()
