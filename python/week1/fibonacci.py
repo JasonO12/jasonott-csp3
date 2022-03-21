85b4606ef9836f1fbb3b34ffbc5ce1da2e7f6c3d
Jason Ott <89180255+JasonO12@users.noreply.github.com>
2022-03-21 18:49:05 +0000

    comments fib

--- python/week1/fibonacci.py
+++ python/week1/fibonacci.py
@@ -1,21 +1,26 @@
 def fibonacci(n):
+  # sets the first two values in the fibonacci sequence to remove errors
   a = 1
   print(a)
   b = 1
   print(b)
   i = 2
+   # starts the loop to display each value in the fibonacci sequence for n
   while i < n:
+    # introduces k variable to act as a temp
     k = b
     b = a + b
     a = k
     print(b)
-    
+
+    # iterates each integer until n
     i += 1
   return
 
 def fibonacci_tester():
   n = int(input("Enter Value for Fibonoacci: "))
   fibonacci(n)
+  # runs the function
   
 
 
