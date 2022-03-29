0698d2a8ec0f5c821774018bcc8632c5c1f98413
somaditya1 <89167149+somaditya1@users.noreply.github.com>
2022-03-29 20:50:14 +0000

    Worked on Animation

--- python/week0/animation.py
+++ python/week0/animation.py
@@ -25,7 +25,7 @@ def ship_print(position):
     print(sp + "    |/   ")
     print(SHIP_COLOR, end="")
     print(sp + "\__ |__/ ")
-    print(sp + " #########  ")
+    print(sp + " \____/  ")
     print(RESET_COLOR)
 
 
